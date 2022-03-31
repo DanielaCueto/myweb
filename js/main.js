b6e2b694f6841a60ad0df25b8da893f909cb4f90
Daniela <danisalazarcueto7@gmail.com>
2022-03-31 14:06:15 +0100

    add dark mode and interaction with js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
 'use strict'; 
 
+let body = document.querySelector('body');
+let button = document.querySelector('.buttonmode'); 
+
+function clickedButton(){
+    let newMode = body.classList.toggle('darkmode')
+    console.log(newMode);
+    return newMode;
+}
+
+
+button.addEventListener('click', clickedButton)
