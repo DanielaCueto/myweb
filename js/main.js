53211d20e8de71b567b2300dbaaf88d49697edb9
Daniela <danisalazarcueto7@gmail.com>
2023-02-13 18:01:21 +0000

    change styles

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 'use strict'; 
 
 let body = document.querySelector('body');
-let button = document.querySelector('.buttonmode'); 
+let button = document.querySelector('.checkbox'); 
 
 function clickedButton(){
     let newMode = body.classList.toggle('darkmode')
     console.log(newMode);
+    localStorage.setItem('darkmode', newMode)
     return newMode;
+}
+
+const isDarkMode = localStorage.getItem('darkmode')
+if(isDarkMode === 'true'){
+    body.classList.add('darkmode');
 }
 
 
 button.addEventListener('click', clickedButton)
+
